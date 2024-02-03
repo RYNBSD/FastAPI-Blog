e65984731de8e7f08e9c1807e274886261f0aa36
RYN BSD <rynbsd04@gmail.com>
2024-02-03 06:26:06 +0100

    fix

--- src/util/csrf.py
+++ src/util/csrf.py
@@ -12,5 +12,4 @@ def new_csrf(length: int = 12) -> dict:
 
 
 def verify_csrf(secret: str, token: str) -> bool:
-    hash = md5(secret).digest().decode()
-    return hash == token
+    return md5(secret).digest().decode() == token
